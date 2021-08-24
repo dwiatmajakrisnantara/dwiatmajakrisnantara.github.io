95d25b666ca9627b44a8cf2e134fa9fc842eb0ba
dwiatmajakrisnantara <atmajakrisnantara@gmail.com>
2021-08-25 04:31:38 +0700

    personal website

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dwi Atmaja Krisnantara ![GitHub stars](https://img.shields.io/github/stars/dwiatmajakrisnantara/dwiatmajakrisnantara.github.io.svg?style=social) ![GitHub repo size](https://img.shields.io/github/repo-size/dwiatmajakrisnantara/dwiatmajakrisnantara.github.io.svg?style=popout-square) ![GitHub](https://img.shields.io/github/license/dwiatmajakrisnantara/dwiatmajakrisnantara.github.io.svg?style=popout-square) 
+
+### Free to use Portfolio Templates
+
+For view website [here](https://dwiatmajakrisnantara.github.io)
---
+++ index.js
@@ -0,0 +1,53 @@
+function showabout(){
+    $("#about_container").css("display","inherit");
+    $("#about_container").addClass("animated slideInLeft");
+    setTimeout(function(){
+        $("#about_container").removeClass("animated slideInLeft");
+    },800);
+}
+function closeabout(){
+    $("#about_container").addClass("animated slideOutLeft");
+    setTimeout(function(){
+        $("#about_container").removeClass("animated slideOutLeft");
+        $("#about_container").css("display","none");
+    },800);
+}
+function showinfo(){
+    $("#info_container").css("display","inherit");
+    $("#info_container").addClass("animated slideInRight");
+    setTimeout(function(){
+        $("#info_container").removeClass("animated slideInRight");
+    },800);
+}
+function closeinfo(){
+    $("#info_container").addClass("animated slideOutRight");
+    setTimeout(function(){
+        $("#info_container").removeClass("animated slideOutRight");
+        $("#info_container").css("display","none");
+    },800);
+}
+function showcontact(){
+    $("#contact_container").css("display","inherit");
+    $("#contact_container").addClass("animated slideInUp");
+    setTimeout(function(){
+        $("#contact_container").removeClass("animated slideInUp");
+    },800);
+}
+function closecontact(){
+    $("#contact_container").addClass("animated slideOutDown");
+    setTimeout(function(){
+        $("#contact_container").removeClass("animated slideOutDown");
+        $("#contact_container").css("display","none");
+    },800);
+}
+setTimeout(function(){
+    $("#loading").addClass("animated fadeOut");
+    setTimeout(function(){
+      $("#loading").removeClass("animated fadeOut");
+      $("#loading").css("display","none");
+      $("#box").css("display","none");
+      $("#about").removeClass("animated fadeIn");
+      $("#contact").removeClass("animated fadeIn");
+      $("#info").removeClass("animated fadeIn");
+    },1000);
+},1500);
