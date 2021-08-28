e6dea80b9289ebcfc6ebfc5ee0b1c51521b01205
Dwi Atmaja Krisnantara <atmajakrisnantara@gmail.com>
2021-08-28 21:28:29 +0700

    personal website

---
+++ mail.php
@@ -0,0 +1,14 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['emailaddress'];
+$message= $_POST['message'];
+$to = "atmajakrisnantara@gmail.com";
+$subject = "Mail From Website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@atmajakrisnantara.my.id" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+?>
