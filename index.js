f696525313984a03021f16f2cacc2c7a304c09bc
Dwi Atmaja Krisnantara <atmajakrisnantara@gmail.com>
2022-03-19 01:24:54 +0700

    Update Commit

--- index.js
+++ index.js
@@ -1,111 +1,53 @@
-function showabout(){
-    $("#about_container").css("display","inherit");
-    $("#about_container").addClass("animated slideInLeft");
-    setTimeout(function(){
-        $("#about_container").removeClass("animated slideInLeft");
-    },800);
+function showabout() {
+  $("#about_container").css("display", "inherit");
+  $("#about_container").addClass("animated slideInLeft");
+  setTimeout(function () {
+    $("#about_container").removeClass("animated slideInLeft");
+  }, 800);
 }
-function closeabout(){
-    $("#about_container").addClass("animated slideOutLeft");
-    setTimeout(function(){
-        $("#about_container").removeClass("animated slideOutLeft");
-        $("#about_container").css("display","none");
-    },800);
+function closeabout() {
+  $("#about_container").addClass("animated slideOutLeft");
+  setTimeout(function () {
+    $("#about_container").removeClass("animated slideOutLeft");
+    $("#about_container").css("display", "none");
+  }, 800);
 }
-function showinfo(){
-    $("#info_container").css("display","inherit");
-    $("#info_container").addClass("animated slideInRight");
-    setTimeout(function(){
-        $("#info_container").removeClass("animated slideInRight");
-    },800);
+function showinfo() {
+  $("#info_container").css("display", "inherit");
+  $("#info_container").addClass("animated slideInRight");
+  setTimeout(function () {
+    $("#info_container").removeClass("animated slideInRight");
+  }, 800);
 }
-function closeinfo(){
-    $("#info_container").addClass("animated slideOutRight");
-    setTimeout(function(){
-        $("#info_container").removeClass("animated slideOutRight");
-        $("#info_container").css("display","none");
-    },800);
+function closeinfo() {
+  $("#info_container").addClass("animated slideOutRight");
+  setTimeout(function () {
+    $("#info_container").removeClass("animated slideOutRight");
+    $("#info_container").css("display", "none");
+  }, 800);
 }
-function showcontact(){
-    $("#contact_container").css("display","inherit");
-    $("#contact_container").addClass("animated slideInUp");
-    setTimeout(function(){
-        $("#contact_container").removeClass("animated slideInUp");
-    },800);
+function showcontact() {
+  $("#contact_container").css("display", "inherit");
+  $("#contact_container").addClass("animated slideInUp");
+  setTimeout(function () {
+    $("#contact_container").removeClass("animated slideInUp");
+  }, 800);
 }
-function closecontact(){
-    $("#contact_container").addClass("animated slideOutDown");
-    setTimeout(function(){
-        $("#contact_container").removeClass("animated slideOutDown");
-        $("#contact_container").css("display","none");
-    },800);
+function closecontact() {
+  $("#contact_container").addClass("animated slideOutDown");
+  setTimeout(function () {
+    $("#contact_container").removeClass("animated slideOutDown");
+    $("#contact_container").css("display", "none");
+  }, 800);
 }
-setTimeout(function(){
-    $("#loading").addClass("animated fadeOut");
-    setTimeout(function(){
-      $("#loading").removeClass("animated fadeOut");
-      $("#loading").css("display","none");
-      $("#box").css("display","none");
-      $("#about").removeClass("animated fadeIn");
-      $("#contact").removeClass("animated fadeIn");
-      $("#info").removeClass("animated fadeIn");
-    },1000);
-},1500);
-
-// // Typewriter
-// var TxtType = function(el, toRotate, period) {
-//     this.toRotate = toRotate;
-//     this.el = el;
-//     this.loopNum = 0;
-//     this.period = parseInt(period, 10) || 2000;
-//     this.txt = '';
-//     this.tick();
-//     this.isDeleting = false;
-// };
-
-// TxtType.prototype.tick = function() {
-//     var i = this.loopNum % this.toRotate.length;
-//     var fullTxt = this.toRotate[i];
-
-//     if (this.isDeleting) {
-//     this.txt = fullTxt.substring(0, this.txt.length - 1);
-//     } else {
-//     this.txt = fullTxt.substring(0, this.txt.length + 1);
-//     }
-
-//     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-//     var that = this;
-//     var delta = 200 - Math.random() * 100;
-
-//     if (this.isDeleting) { delta /= 2; }
-
-//     if (!this.isDeleting && this.txt === fullTxt) {
-//     delta = this.period;
-//     this.isDeleting = true;
-//     } else if (this.isDeleting && this.txt === '') {
-//     this.isDeleting = false;
-//     this.loopNum++;
-//     delta = 500;
-//     }
-
-//     setTimeout(function() {
-//     that.tick();
-//     }, delta);
-// };
-
-// window.onload = function() {
-//     var elements = document.getElementsByClassName('typewrite');
-//     for (var i=0; i<elements.length; i++) {
-//         var toRotate = elements[i].getAttribute('data-type');
-//         var period = elements[i].getAttribute('data-period');
-//         if (toRotate) {
-//           new TxtType(elements[i], JSON.parse(toRotate), period);
-//         }
-//     }
-//     // INJECT CSS
-//     var css = document.createElement("style");
-//     css.type = "text/css";
-//     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-//     document.body.appendChild(css);
-// };
+setTimeout(function () {
+  $("#loading").addClass("animated fadeOut");
+  setTimeout(function () {
+    $("#loading").removeClass("animated fadeOut");
+    $("#loading").css("display", "none");
+    $("#box").css("display", "none");
+    $("#about").removeClass("animated fadeIn");
+    $("#contact").removeClass("animated fadeIn");
+    $("#info").removeClass("animated fadeIn");
+  }, 1000);
+}, 1500);
